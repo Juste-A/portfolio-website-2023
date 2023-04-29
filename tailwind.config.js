8ea3becccc350c27995ffd82f3bec45379c61b6f
Juste Astrauskaite <juste.astrauskaite@gmail.com>
2023-04-29 21:14:34 +0100

    basic configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ] /* inside conten we add paths to folders we will use */,
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
